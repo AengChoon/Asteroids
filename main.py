1638bce515710e7314ce813fd49a1de71a8f320c
AengChoon <kevin1131@naver.com>
2024-08-20 09:29:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-
-# import everything from constants
-# into the current file
 from constants import *
+import player
 
 def main():
     pygame.init()
+
     clock = pygame.time.Clock()
     delta_time = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    my_player = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        my_player.draw(screen)
+
         pygame.display.flip()
         delta_time = clock.tick(60) / 1000
 
